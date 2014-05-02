9d18f7054136f9ba7b8916d2ef4c5691986b690f
Sean <smreidy113@gmail.com>
2014-05-02 16:45:48 -0400

    blah

--- Data.py
+++ Data.py
@@ -1,4 +1,4 @@
-attrs = {"Course Quality": r'CourseQuality', "Instructor Quality": r'InstructorQuality', "Difficulty": r'Difficulty', "Communication Ability": r'CommAbility'}
+attrs = {"Course Quality": r'CourseQuality', "Instructor Quality": r'InstructorQuality', "Difficulty": r'Difficulty', "Communication Ability": r'CommAbility', "Amount Learned": r'AmountLearned', "Work Required": r'WorkRequired'}
 majors = ["Accounting", "Business Economic and Public Policy"]
 minors = ["N/A", "Computer Science"]
 
