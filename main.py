e7719c46199001213d37f252e0d0507422e43910
Sean <smreidy113@gmail.com>
2014-05-02 15:30:43 -0400

    initial commit
    hi

---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+import oauth2 as oauth
+import base64
+import json
+from flask import Flask
+from flask import jsonify
+from flask import request
+from requests.adapters import HTTPAdapter
+
+api_key = 'q2cm13OPDbZAzvJHGxWgtqcI1ZFp6K'
+params = {'count': str(100)}
+s = requests.Session()
+s.mount('http://', HTTPAdapter(max_retries=5))
+
+app = Flask(__name__)
+
+req = requests.get('http://api.penncoursereview.com/v1/depts?token=' + api_key)
+
+
+def startCode():
+	ans = ""
+	ans = "<html>"
+	ans += "\n\t<head>"
+	ans += "\n\t\t<title>Penn Course Helper</title>"
+	ans += "\n\t</head>"
+	ans += "\n\t<body>"
+	return ans
+
+def endCode():
+	ans = ""
+	ans += "\n\t</body>"
+	ans += "\n</html>"
+	return ans
+
+
+@app.route('/')
+def start():	
+	js = req.json()['result']
+	html = ""
+	html += startCode()
+	html += "\n\t\tSelect a department:"
+	html += "\n\t\t<form name=\"myform\" action=\"listcourses\" method=\"POST\">"
+	html += "\n\t\t\t<select name=\"course1\">"
+	print js.keys()
+	for dept in js['values']:
+		html += "\n\t\t\t\t<option value=\"" + dept['id'] + "\">" + dept['id'] + " - " + dept['name'] + "</option>"
+	html += "\n\t\t\t</select>"
+	html += "\n\t\t<input type=\"submit\" value=\"Submit\">"
+	html += "\n\t\t</form>"
+	html += endCode()
+	return html
+
+@app.route('/listcourses', methods=['POST'])
+def listcourses():
+	print request
+	html = ""
+	html += startCode()
+	html += request.form['course1']
+	html += endCode()
+	return html
+
+#req = requests.get('http://api.penncoursereview.com/v1/coursehistories/CIS-110?token=' + api_key)
+
+#print req.text
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ run.sh
@@ -0,0 +1,2 @@
+cd "C:\Users\Sean\Documents\Github\PennCourseHelper"
+python main.py
