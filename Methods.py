a116fcc12246245de000712e3844f807e7c7a991
Sean <smreidy113@gmail.com>
2014-05-03 15:18:37 -0400

    more stupid stuff

--- Methods.py
+++ Methods.py
@@ -75,7 +75,6 @@ def rankedCoursesMultiple(l,p1,p2,p3, taken):
 		revinfo = requests.get('http://api.penncoursereview.com/v1/depts/' + dept + '/reviews?token=' + api_key).json()['result']['values']
 		s.extend(rankedCourses(revinfo,p1,p2,p3))
 	s.sort(key=lambda x:x[1][0], reverse=True)
-	s.sort(key=lambda x:x[1])
 	for course in taken:
 		try:
 			s.remove(course)
