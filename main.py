476f0a60dc7e657168727762511757081f667506
Sean <smreidy113@gmail.com>
2014-05-03 13:10:12 -0400

    lots of stuff

--- main.py
+++ main.py
@@ -100,9 +100,37 @@ def choose_course():
 def listcourses():
 	html = ""
 	html += startCode()
-	html += request.form['dept1']
-	for course in Methods.courseList(request.form['coursestaken']):
-		html += course[0]
+	req1 = requests.get('http://api.penncoursereview.com/v1/depts/' + request.form['dept1'] + '/reviews?token=' + api_key)
+	p1 = request.form['priority1']
+	p2 = request.form['priority2']
+	p3 = request.form['priority3']
+	revinfo = req1.json()['result']['values']
+	courseDict = {}
+	ratingsDict = {}
+	for course in revinfo:
+		courseName = course['section']['primary_alias']
+		if courseName[0:-4] in courseDict.keys():
+			courseDict[courseName[0:-4]].append(course)
+		else:
+			courseDict[courseName[0:-4]] = [course]
+			ratingsDict[courseName[0:-4]] = 0.0
+	for course in courseDict.keys():
+		sumRating1, sumRating2, sumRating3 = 0.0, 0.0, 0.0
+		for section in courseDict[course]:
+			sumRating1 += float(section['ratings'].get(str('r'+p1),0.0))
+			sumRating2 += float(section['ratings'].get(str('r'+p2),0.0))
+			sumRating3 += float(section['ratings'].get(str('r'+p3),0.0))
+		avgRating1 = sumRating1/len(courseDict[course])
+		avgRating2 = sumRating2/len(courseDict[course])
+		avgRating3 = sumRating3/len(courseDict[course])
+		ratingsDict[course] = (3*avgRating1**2 + 2*avgRating2**2 + 1*avgRating3**2) / 96 * 10
+	print ratingsDict.items()
+	s = ratingsDict.items()
+	s.sort(key=lambda x:x[1])
+	for course in s:
+		html += "<br>" + course[0] + " " + str(course[1])
+	#for course in Methods.courseList(request.form['coursestaken']):
+	#	html += course[0]
 	html += endCode()
 	return html
 
